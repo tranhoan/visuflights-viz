c79b3f94e661d98af6c9044ed6a67989ba2f3a62
tranhoan@fel.cvut.cz <tranhoan@fel.cvut.cz>
2020-06-09 13:27:12 +0200

    fixed cors

--- src/main/java/fel/viz/hoang/airplaines/rest/DataController.java
+++ src/main/java/fel/viz/hoang/airplaines/rest/DataController.java
@@ -1,6 +1,7 @@
 package fel.viz.hoang.airplaines.rest;
 import fel.viz.hoang.airplaines.data.DataParser;
 import fel.viz.hoang.airplaines.data.graph.Graph;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataController {
 
     @GetMapping("/data")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Graph getData(){
         return DataParser.getData();
     }
